5815ebcb7ac572d353d5d1f52a8c97b27a5a4e67
linx02 <elvius.linus@gmail.com>
2023-07-27 23:19:12 +0200

    Add docstring to run.py

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import print_output
 import functions
 
-# Main loop of the program
 def main():
+    """
+    Main function of the program.
+
+    Returns:
+        None: The function does not return any value. Runs until user exits the program.
+    """
     print_output.print_main()
     while True:
         command = input('YourStore > ')
